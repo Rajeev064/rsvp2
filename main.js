d8e5111b344e7eb781d283fde9ba2e4f8be5dd7d
Rajeev <59364082+Rajeev064@users.noreply.github.com>
2020-03-03 06:52:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-
-  var firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyBr6J1htv02gy9yShUanoSYb55GEWjiWmE",
     authDomain: "rsvpdemo064.firebaseapp.com",
     databaseURL: "https://rsvpdemo064.firebaseio.com",
@@ -39,12 +38,46 @@
     // Clear form
     document.getElementById('contactForm').reset();
   }
+  function castvote() {
+    /*var mySelect = document.getElementById("events"); 
+    alert(mySelect.options[mySelect.selectedIndex].value); 
+    */var x = document.getElementById("events").value;
+   var img = document.getElementById("btn");
+   
+     var a =document.getElementById("a");
+    
+   if(x === "MINECRAFT")
+    { 
+     a.href= 'https://calendar.google.com/event?action=TEMPLATE&tmeid=NTJiM25sOTltODM1M2NjcnEwZHRzMm43NmYgcnMzMTY3MTk2QG0&tmsrc=rs3167196%40gmail.com';
+      a.appendchild(img);
+    x.appendchild(a);
+     
+    }
+    if(x === "TREASURE HUNT")
+    { 
+      
+     a.href ='https://calendar.google.com/event?action=TEMPLATE&tmeid=MmdvOXYyNGcwdXNjNWk2YTZncjJkdjJvcXEgcnMzMTY3MTk2QG0&tmsrc=rs3167196%40gmail.com';
+      a.appendchild(img);
+    x.appendchild(a);
+    }
+    if(x === "MINUTE TO WIN IT")
+    { 
+      a.href ='https://calendar.google.com/event?action=TEMPLATE&tmeid=Nm9xZmdwcGVmZWhyanZnNHNpcjB0MWFsNTEgcnMzMTY3MTk2QG0&tmsrc=rs3167196%40gmail.com';
+    a.appendchild(img);
+    x.appendchild(a);
+    }
+    if(x === "ALL EVENTS")
+    { 
+      a.href='https://calendar.google.com/event?action=TEMPLATE&tmeid=NDliaTEwZXFrdGlpam00MjlmcDYwYmhhaWUgcnMzMTY3MTk2QG0&tmsrc=rs3167196%40gmail.com';
+         a.appendchild(img);
+    x.appendchild(a);
+    }
+}
   
   // Function to get get form values
   function getInputVal(id){
     return document.getElementById(id).value;
   }
-  
   // Save message to firebase
   function saveMessage(name, email, phone){
     var newMessageRef = messagesRef.push();
